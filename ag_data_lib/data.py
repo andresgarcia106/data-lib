b1deaace9a2d93e1329b0bc06fa25754a01a6cea
Andres Garcia <jgarciaf106@gmail.com>
2022-11-02 16:02:11 -0600

    Refactor Functions and Remove unused code

--- ag_data_lib/data.py
+++ ag_data_lib/data.py
@@ -1,68 +1,54 @@
 # library imports
 import os
-import codecs
 import json
 import win32com.client
-import datetime as dt
 import pandas as pd
 import xlwings as xw
 from xlwings.utils import rgb_to_int
-import fiscalyear as fy
 from .utils import *
 from sqlalchemy import create_engine
-from pptx import Presentation
 
 
 class DB:
     def __init__(self, db_config):
+
         self._cfg = db_config
 
     def set_engine(self):
         """
-        Create a connection to the database based on the database type
-        :return: The engine object.
+        It takes a dictionary of connection strings and returns the connection string for the database
+        type specified in the config file
+        :return: The create_engine function is being returned.
         """
-        if self._cfg["type"] == "postgres":
-            return create_engine(
-                self._cfg["connstring"].format(
-                    self._cfg["user"],
-                    self._cfg["password"],
-                    self._cfg["server"],
-                    self._cfg["database"],
-                )
-            )
-        elif self._cfg["type"] == "mssql":
-            return create_engine(
-                self._cfg["connstring"].format(
-                    self._cfg["server"], self._cfg["database"]
-                )
-            )
 
-        elif self._cfg["type"] == "oracle":
-            return create_engine(
-                self._cfg["connstring"].format(
-                    self._cfg["user"],
-                    self._cfg["password"],
-                    self._cfg["server"],
-                    self._cfg["database"],
-                )
-            )
-        elif self._cfg["type"] == "mysql":
-            return create_engine(
-                self._cfg["connstring"].format(
-                    self._cfg["user"],
-                    self._cfg["password"],
-                    self._cfg["server"],
-                    self._cfg["database"],
-                )
-            )
-        elif self._cfg["type"] == "sqlite":
-            return create_engine(
-                self._cfg["connstring"].format(
-                    self._cfg["server"],
-                    self._cfg["database"],
-                )
-            )
+        engines = {
+            "postgres": self._cfg["connstring"].format(
+                self._cfg["user"],
+                self._cfg["password"],
+                self._cfg["server"],
+                self._cfg["database"],
+            ),
+            "mssql": self._cfg["connstring"].format(
+                self._cfg["server"], self._cfg["database"]
+            ),
+            "oracle": self._cfg["connstring"].format(
+                self._cfg["user"],
+                self._cfg["password"],
+                self._cfg["server"],
+                self._cfg["database"],
+            ),
+            "mysql": self._cfg["connstring"].format(
+                self._cfg["user"],
+                self._cfg["password"],
+                self._cfg["server"],
+                self._cfg["database"],
+            ),
+            "sqlite": self._cfg["connstring"].format(
+                self._cfg["server"], self._cfg["database"]
+            ),
+        }
+
+        return create_engine(engines.get(self._cfg["type"]))
 
 
 class Data:
@@ -77,14 +63,6 @@ class Data:
         self._query_path = None
         self._input_path = None
         self._pass_path = None
-        self._today = dt.datetime.today()
-        self._monthname_long = self._today.strftime("%B")
-        self._monthname_short = self._today.strftime("%b")
-        self._monthyear = self._today.strftime("%m%y")
-        self._fiscal_year = None
-        self._current_quarter = None
-        self._fiscal_quarter = None
-        self._set_fiscal_year()
 
     def set_path(
         self,
@@ -121,31 +99,6 @@ class Data:
                 self._query_path = paths[2]
                 self._pass_path = paths[3]
 
-    def _set_fiscal_year(self, f_year=None, s_month=11, s_day=1, s_year="previous"):
-        """
-        :param f_year: The year you want to set as the fiscal year
-        :param s_month: The starting month of the fiscal year, defaults to 11 (optional)
-        :param s_day: The day of the month that the fiscal year starts, defaults to 1 (optional)
-        :param s_year: The start year of the fiscal year, defaults to previous (optional)
-        """
-
-        if f_year is None:
-            if dt.datetime.today().month < 11:
-                f_year = dt.datetime.today().year - 1
-            else:
-                f_year = dt.datetime.today().year
-
-        self._fiscal_year = fy.FiscalYear(f_year)
-        fy.setup_fiscal_calendar(
-            start_year=s_year, start_month=s_month, start_day=s_day
-        )
-        self._fiscal_quarter = str(
-            fy.FiscalQuarter.current().prev_fiscal_quarter
-        ).split(" ")[0]
-        self._current_quarter = str(
-            fy.FiscalQuarter.current().prev_fiscal_quarter
-        ).split(" ")[1]
-
     def run_sql_query(self, query, db_engine, **kwargs):
         """
         If the query is a file, then read the file and pass it to the database engine. If the query is a
@@ -155,20 +108,23 @@ class Data:
         :param db_engine: the database engine
         :return: A dataframe
         """
+        out_df = pd.DataFrame()
 
         if os.path.exists(self._query_path + f"{query}.sql"):
             if kwargs == {}:
                 file_query = open(self._query_path + f"{query}.sql").read()
-                return pd.read_sql_query(file_query, db_engine)
+                out_df = pd.read_sql_query(file_query, db_engine)
             else:
                 with open(self._query_path + f"{query}.sql") as get:
                     file_query = get.read()
-                return pd.read_sql_query(file_query.format(**kwargs), db_engine)
+                out_df = pd.read_sql_query(file_query.format(**kwargs), db_engine)
         else:
             if kwargs == {}:
-                return pd.read_sql_query(query, db_engine)
+                out_df = pd.read_sql_query(query, db_engine)
             else:
-                return pd.read_sql_query(query.format(**kwargs), db_engine)
+                out_df = pd.read_sql_query(query.format(**kwargs), db_engine)
+
+        return out_df
 
     def execute_sql_query(self, query, db_engine):
         """
@@ -189,12 +145,12 @@ class Data:
         :param file_path: The path to the file to read
         :return: the reader function.
         """
-        df = (
+        out_df = (
             reader(self._input_path + file_path, **kwargs)
             if os.path.exists(self._input_path + file_path)
             else reader(file_path, **kwargs)
         )
-        return df
+        return out_df
 
     def _password_tracker(self, request_date, output_file_name, password):
         """
@@ -227,134 +183,7 @@ class Data:
         with open(self._pass_path + file_name, "w") as json_file:
             json.dump(json_obj, json_file, indent=4, separators=(",", ": "))
 
-    # TODO: Add a function to send email with password after email with attachment is sent
-    # def _password_share(self, password, email_subject):
-    #     """
-    #     :param password: The password to be shared
-    #     :param email_subject: The subject of the email that will be sent to the user
-    #     """
-
-    #     outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
-    #     sent_items = outlook.GetDefaultFolder(5)
-    #     messages = sent_items.Items
-    #     message = messages.GetLast()
-
-    #     print(password, email_subject, message)
-
-    #     pass
-
-    def _draft_report_to_email(
-        self, recipient, file_name, password=None, folder_search=None
-    ):
-        """
-        :param recipient: The email address of the recipient
-        :param file_name: The name of the file that will be attached to the email
-        :param password: The password to protect the file with
-        :param folder_search: The name of the folder to search for the email. If None, the search will
-        be done in the inbox
-        :return: None
-        """
-
-        OUTLOOK = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
-        CURRENT_USER = (
-            OUTLOOK.CurrentUser.AddressEntry.GetExchangeUser().PrimarySmtpAddress
-        )
-
-        if folder_search is not None:
-            inbox = OUTLOOK.GetDefaultFolder(6).Folders.Item(
-                folder_search
-            )  # inbox in sub-folders
-        else:
-            inbox = OUTLOOK.GetDefaultFolder(6)  # inbox emails
-
-        # instantiate outlook to send email
-        const = win32com.client.constants
-        ol_mail_item = 0x0
-        obj = win32com.client.Dispatch("Outlook.Application")
-
-        # get received emails
-        messages = inbox.Items
-        message = messages.GetLast()
-        sender_name = message.Sender.GetExchangeUser().name.split(",")[-1].strip()
-        sender_address = message.Sender.GetExchangeUser().PrimarySmtpAddress
-        sender_cc_address = ";".join(
-            [
-                item.AddressEntry.GetExchangeUser().PrimarySmtpAddress
-                for item in message.Recipients
-                if item.AddressEntry.GetExchangeUser().PrimarySmtpAddress
-                != CURRENT_USER
-            ]
-        )
-
-        # set email signature
-        signature_htm = os.path.join(
-            os.environ["USERPROFILE"], "AppData\\Roaming\\Microsoft\\Signatures\\HP.htm"
-        )
-        html_file = codecs.open(signature_htm, "r", "utf-8", errors="ignore")
-        email_signature = html_file.read()
-        html_file.close()
-
-        # get original email subject
-        email_subject = message.Subject
-
-        # create new email items
-        new_mail = obj.CreateItem(ol_mail_item)
-        new_mail.Subject = email_subject
-        new_mail.BodyFormat = 2
-
-        # html email body and subject
-        if password is not None:
-            email_body = """
-                    <HTML>
-                        <BODY style="font-family:Forma DJR Micro (Body);font-size:14.5px;">
-                            <p>Hi {0},</p>
-                            <p>Please find attached the report requested.</p>
-                            <p>
-                            The attached file is password protected, password will be shared on the following email.
-                            </p>
-                            <p><em style="color:red">*** Delete Before Sending Email ***</em>
-                            <strong>Password:</strong> {1} Copy password to send it on a separate email.
-                            <em style="color:red">*** Delete Before Sending Email ***</em></p>
-                            <p>Kind Regards,</p> 
-                        </BODY>
-                    </HTML>""".format(
-                sender_name, password
-            )
-        else:
-            email_body = """
-                <HTML>
-                    <BODY style="font-family:Forma DJR Micro (Body);font-size:14.5px;">
-                        <p>Hi {0},</p>
-                        <p>Please find attached the report requested.</p>
-                        <p>Kind Regards,</p> 
-                    </BODY>
-                </HTML>""".format(
-                sender_name
-            )
-
-        # validate that email will be sent to correct recipient
-        if sender_address == recipient:  # based on the subject replying to email
-            reply_all = message.ReplyAll()
-            new_mail.HTMLBody = email_body + email_signature + reply_all.HTMLBody
-            new_mail.To = sender_address
-            new_mail.CC = sender_cc_address
-            attachment = self._output_path + file_name
-            new_mail.Attachments.Add(Source=attachment)
-            new_mail.save()
-
-        return None
-
-    def _file_saver(
-        self,
-        data,
-        file_name,
-        protect_file,
-        security_method,
-        auth_users,
-        draft_email,
-        requester,
-        email_folder,
-    ):
+    def _file_saver(self, data, file_name, protect_file, security_method, auth_users):
         """
         :param data: The data to be written to the Excel file
         :param file_name: The name of the file to be saved
@@ -412,7 +241,7 @@ class Data:
                     book.SaveAs(output_path, 51, file_password)
                     book.Close()
                     excel.Application.Quit()
-                    
+
                     # update password tracker
                     self._password_tracker(
                         self._today,
@@ -420,7 +249,7 @@ class Data:
                         file_password,
                     )
 
-                if security_method == "RMS":                    
+                if security_method == "RMS":
                     active_book = xw.books.active
                     for auth_user in auth_users:
                         active_book.api.Permission.Add(auth_user, 15)
@@ -435,10 +264,6 @@ class Data:
         except Exception as e:
             raise
 
-        # prompt
-        if draft_email:
-            self._draft_email(requester, file_name, file_password, email_folder)
-
     def export_data(
         self,
         odf,
@@ -446,115 +271,9 @@ class Data:
         protect_file=False,
         security_method=None,
         auth_users=[],
-        draft_email=False,
-        requester=None,
-        email_folder=None,
     ):
-        """
-        :param odf: the dataframe to be exported
-        :param custom_filename: If you want to save the file with a custom name, you can do so
-        :param protect_file: If True, the file will be protected with a password, defaults to False
-        (optional)
-        :param draft_email: If True, the email will be saved as a draft in Outlook, defaults to False
-        (optional)
-        :param requester: The email address of the person who requested the report
-        :param email_folder: The name of the folder in which to save the email. If None, the email will
-        be saved in the root folder
-        """
-
         # assign file name
-        if custom_filename is None:
-            file_name = "Output Report " + self._today.strftime("%Y-%m-%d") + ".xlsb"
-        else:
-            file_name = custom_filename
+        file_name = file_namer(custom_filename)
 
         # save file
-        self._file_saver(
-            odf,
-            file_name,
-            protect_file,
-            security_method,
-            auth_users,
-            draft_email,
-            requester,
-            email_folder,
-        )
-
-    # TODO: add a method to send the email to DEI, Planners and PLT Once quarterly report is ready.
-    def quarterly_email_sender(self, roster_df, file_name, email_round):
-
-        pass
-
-    # TODO: Review need of function to create PPTX to avoid manual PPTX creation
-    # def ppt_export(self, file_type, template_type, org=None):
-    #     """
-    #     :param file_type: "HPI" or "L1 ORG"
-    #     :param template_type: The template type is either "HPI" or "L1 ORG"
-    #     :param org: The name of the organization you want to export
-    #     """
-
-    #     # determine output file HPI Total or L1 Org Dashboard
-    #     if file_type == "HPI":
-    #         output_file = "HPI DEI Dashboard {0} {1}".format(
-    #             self._current_quarter, self._fiscal_quarter
-    #         )
-    #     elif file_type == "L1 ORG":
-    #         output_file = "DEI {0} Dashboard {1}".format(org, self._monthyear)
-
-    #     # set slides to be updated
-    #     prs = Presentation(
-    #         "../Templates/HP_Presentation_Template_" + template_type + ".pptx"
-    #     )
-    #     slide_1 = prs.slides[0]
-    #     slide_4 = prs.slides[3]
-    #     slide_6 = prs.slides[5]
-    #     slide_7 = prs.slides[6]
-    #     slide_8 = prs.slides[7]
-    #     slide_9 = prs.slides[8]
-
-    #     # set placeholders to be updated
-    #     prs_sub_1 = slide_1.placeholders[1]
-    #     prs_sub_4 = slide_4.placeholders[0]
-    #     prs_sub_6 = slide_6.placeholders[0]
-    #     prs_sub_7 = slide_7.placeholders[0]
-    #     prs_sub_8 = slide_8.placeholders[0]
-    #     prs_sub_9 = slide_9.placeholders[0]
-
-    #     # update placeholders
-    #     if file_type == "HPI":
-    #         prs_sub_1.text = "As of {0} month, end/{1}".format(
-    #             self._monthname_long, self._current_quarter
-    #         )
-    #     elif file_type == "L1 ORG":
-    #         prs_sub_1.text = "{0} (As of {1} month, end/{2})".format(
-    #             org, self._monthname_long, self._current_quarter
-    #         )
-
-    #     prs_sub_4.text = (
-    #         self._fiscal_quarter + " " + self._current_quarter + " Headcount"
-    #     )
-
-    #     prs_sub_6.text = "{0}/{1} Status to Diversity Targets (Company Level)".format(
-    #         self._monthname_short, self._current_quarter
-    #     )
-    #     prs_sub_7.text = "{0}/{1} Active Headcount by Organization / MRU".format(
-    #         self._monthname_short, self._current_quarter
-    #     )
-    #     prs_sub_8.text = (
-    #         "{0}/{1} Active Headcount by Organization / MRU (Absolute values)".format(
-    #             self._monthname_short, self._current_quarter
-    #         )
-    #     )
-    #     prs_sub_9.text = "{0}/{1} US Ethnic Groups Not Self-Identified HC by Organization / MRU".format(
-    #         self._monthname_short, self._current_quarter
-    #     )
-
-    #     # save updated template
-    #     prs.save(
-    #         self.__output_path
-    #         + "Quarterly Dashboards\\"
-    #         + file_type
-    #         + "\\"
-    #         + output_file
-    #         + ".pptx"
-    #     )
+        self._file_saver(odf, file_name, protect_file, security_method, auth_users)
--- ag_data_lib/utils.py
+++ ag_data_lib/utils.py
@@ -31,12 +31,12 @@ def create_path():
     root = os.path.dirname(os.getcwd())
     target_dir = "02_data"
     paths = ["01_input_files", "02_output_files", "03_query_files", "04_password_files"]
-    
+
     for path in paths:
-        if not os.path.isdir(root + f"\\{target_dir}\\{path}"):   
-            os.mkdir(root + f"\\{target_dir}\\{path}")         
+        if not os.path.isdir(root + f"\\{target_dir}\\{path}"):
+            os.mkdir(root + f"\\{target_dir}\\{path}")
             output_path.append(root + f"\\{target_dir}\\{path}")
-            
+
     return output_path
 
 
@@ -59,23 +59,24 @@ def column_trim(df):
     trim_strings = lambda x: x.strip() if isinstance(x, str) else x
     return df.applymap(trim_strings)
 
+
 def file_format_constant(file_name):
     """
     Returns the file format constant
     """
     extension = file_name.split(".")[-1]
     constant = 0
-    
+
     if extension == "xlsb":
-        constant = 50    
+        constant = 50
     elif extension == "xlsx":
-        constant =  51
+        constant = 51
     elif extension == "xlsm":
         constant = 52
     elif extension == "csv":
-        constant =  62
-        
-    return constant 
+        constant = 62
+
+    return constant
 
 
 def number_to_string(df):
@@ -106,17 +107,27 @@ def password_generator(co_key):
     """
     It takes a string as an argument and returns a string that is the concatenation of the argument and
     the current time
-    
+
     :param co_key: This is the company key that is used to identify the company
     :return: the concatenation of the co_key and the current time.
     """
     return co_key + str(dt.datetime.today().strftime("%I%M%S"))
 
 
+def file_namer(file_name):
+    # assign file name
+    if file_name is None:
+        file_name = "Report " + dt.datetime.today().strftime("%Y-%m-%d") + ".xlsb"
+    else:
+        file_name = file_name
+
+    return file_name
+
+
 def notebook_to_html(notebook_path, html_path):
     """
     It takes a Jupyter notebook and converts it to an HTML file
-    
+
     :param notebook_path: The path to the notebook you want to convert
     :param html_path: The path to the output HTML file
     """
@@ -137,33 +148,12 @@ def notebook_to_html(notebook_path, html_path):
     # second with notebook metadata
     try:
         output = HTMLExporter(config=cfg).from_filename(notebook_path)
-        html_output_name = notebook_path.rsplit('.', 1)[0] + '.html'
+        html_output_name = notebook_path.rsplit(".", 1)[0] + ".html"
         # Write to output html file
         with open(html_path + f"/{html_output_name}", "w", encoding="utf8") as f:
             f.write(output[0])
     except FileNotFoundError:
         print("Notebook not found. Review the Notebook path.")
-
-
-def ppt_identifier(input_file, slide_number):
-    """
-    Identifies a ppt single slide elements
-
-    Parameters
-    ----------
-    input_file : str
-        The ppt template directory location and name
-    slide_number : int
-        The slide number
-    """
-
-    prs = Presentation(input)
-    slide = prs.slides[slide_number - 1]
-    for shape in slide.shapes:
-        print(
-            "id: %s, name: %s, , type: %s"
-            % (shape.shape_id, shape.name, shape.shape_type)
-        )
 
 
 def create_folder_tree(root_folder, sub_dir_folder, folder_list, path):
@@ -185,42 +175,3 @@ def create_folder_tree(root_folder, sub_dir_folder, folder_list, path):
             print(f"Directory {folder} created")
         except FileExistsError:
             print("Directory ", folder, " already exists")
-
-
-def ppt_analyzer(input_path, output_path):
-    """
-    Review a PowerPoint presentation to indicate each of ppt elements
-
-    Parameters
-    ----------
-    input_path : str
-        The ppt template directory location and name
-    output_path : str
-        The file output to avoid overwrite the original template
-    """
-
-    prs = Presentation(input_path)
-
-    for index, _ in enumerate(prs.slide_layouts):
-        slide = prs.slides.add_slide(prs.slide_layouts[index])
-        # Not every slide has to have a title
-        try:
-            title = slide.shapes.title
-            title.text = "Title for Layout {}".format(index)
-        except AttributeError:
-            print("No Title for Layout {}".format(index))
-
-        # Go through all the placeholders and identify them by index and type
-        for shape in slide.placeholders:
-            if shape.is_placeholder:
-                phf = shape.placeholder_format
-                # Do not overwrite the title which is just a special placeholder
-                try:
-                    if "Title" not in shape.text:
-                        shape.text = "Placeholder index:{} type:{}".format(
-                            phf.idx, shape.name
-                        )
-                except AttributeError:
-                    print("{} has no text attribute".format(phf.type))
-                print("{} {}".format(phf.idx, shape.name))
-    prs.save(output_path)
